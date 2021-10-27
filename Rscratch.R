1e3feabea53a19da7a8310f3b9ea0482f16b40a8
Daniel B <danielyho@yahoo.com>
2021-10-27 08:39:35 +0200

    update

--- Rscratch.R
+++ Rscratch.R
@@ -7,3 +7,17 @@ X11()
 plot(iris)
 while(names(dev.cur()) !='null device') Sys.sleep(1)
 
+
+
+X <- seq(-pi, pi, by = 0.2)
+plot(X,sin(X), pch=1:25, cex=2)
+
+
+     X  <-  1:5
+
+
+
+     rep(seq(25, 5, -5),5)
+r <- c(sapply(seq(5, 25, 5), function(i) rep(i, 5)))
+t <- rep(seq(25, 5, -5), 5)
+plot(r,t)
